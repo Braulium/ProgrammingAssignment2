506c5354c0b65f9f52a61e6bc163da76048c3405
Braulium <95143955+Braulium@users.noreply.github.com>
2022-02-05 09:41:32 +0100

    Solution Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to:
+## an object containing a matrix and cacheing its inverse
+## receiving as an input the output of the first object, and calculating the inverse of the matrix if that is not alreadz in cache, or taking it from the cache if it's already computed
 
-## Write a short comment describing this function
+## makeCacheMatrix: given a matrix as an input, will create a list of functions to set and get a matrix, and set and get the inverse of that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolved <- function(solve) m <<- solve
+  getsolved <- function() m
+  list(set = set, get = get,
+       setsolved = setsolved,
+       getsolved = getsolved)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: receives the output of makeCacheMatrix, retrieving inverse of the matrix if it has been alreadz calculated, or calculates it if not
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolved()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolved(m)
+  m
 }
